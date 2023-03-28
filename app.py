03cb701be9c307c8baf12688cfb7c4d0326a816d
Kai Braeunig <kaib21@pm.me>
2023-03-28 12:57:09 +0200

    update comments

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ st.title("Status of Game Changer Tanks for Ukraine")
 st.plotly_chart(fig)
 
 st.markdown("""
-    Last Update: 15.03.2023
+    Last Update: 28.03.2023
 
     Comments:
    
@@ -127,32 +127,7 @@ st.markdown("""
     * 🇸🇪: announced a maximum of 10 Leopard 2, could be less
     
     No responsiblity is taken for the correctness and completeness of this information.\n
+    [Sources and data table](https://docs.google.com/spreadsheets/d/1I6_B_REuG6m8kQFKfVm5LWnrcdOxiB7Imi8FfGGzuZw/edit?usp=sharing)\n
     Created by Kai Braeunig: [LinkedIn profile - login required](https://www.linkedin.com/in/ai-kai/)
     
-""")
-
-
-# Display optional table
-#if st.checkbox("Show Data Table"):
-#    st.write(data)
-    
-# Display optional sources
-if st.checkbox("Show Sources"):
-    st.markdown("""
-* https://english.nv.ua/nation/ukraine-to-receive-no-more-than-50-leopards-tanks-promised-by-west-by-april-newspaper-claims-50305298.html
-* https://www.technology.org/2023/03/11/ukraine-will-receive-twice-as-many-challenger-2-tanks-as-previously-planned/
-* https://www.forbes.com/sites/davidaxe/2023/03/04/more-ex-british-challenger-2-tanks-are-bound-for-ukraine-as-london-doubles-its-pledge/?sh=3445596b232b
-* https://news.yahoo.com/first-batch-leopard-1-tanks-182300105.html
-* https://english.nv.ua/nation/germany-to-transfer-178-leopard-1a5-tanks-to-ukraine-news-50302756.html
-* https://news.yahoo.com/poland-sends-10-more-leopard-161300461.html
-* https://euromaidanpress.com/2023/03/09/poland-already-sent-14-leopard-2-tanks-to-ukraine-as-ukrainian-crews-finished-training-on-them-polish-defense-minister/
-* https://english.nv.ua/nation/tank-coalition-has-full-battalion-of-leopard-2a4-tanks-ready-for-ukraine-news-50304558.html
-* https://www.reuters.com/world/canada-imposes-new-russia-sanctions-pledges-battle-tanks-ukraine-2023-02-24/
-* https://www.swissinfo.ch/eng/reuters/ukrainian-soldiers-wrap-up-leopard-2a4-tank-training-in-spain/48356738
-* https://www.reuters.com/world/europe/ukrainian-soldiers-wrap-up-leopard-2a4-tank-training-spain-2023-03-13/
-* https://www.berliner-zeitung.de/news/offiziell-deutschland-liefert-14-leopard-2-kampfpanzer-in-die-ukraine-li.310692
-* https://www.armyrecognition.com/defense_news_march_2023_global_security_army_industry/germany_and_portugal_to_donate_together_to_ukraine_21_leopard_2a6_tanks.html
-* https://www.reuters.com/world/europe/sweden-send-up-10-leopard-tanks-ukraine-2023-02-24/
-* https://www.reuters.com/world/europe/finland-send-three-leopard-tanks-ukraine-2023-02-23/
-* https://www.welt.de/politik/ausland/article243651059/Ueber-100-Leopard-1-Pistorius-verkuendet-in-Kiew-weitere-Militaerhilfe.html
 """)
